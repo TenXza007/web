d49269dc0bd1076e1e7da337760663e18b7e9bcb
TenXza007 <6421061012@cdti.ac.th>
2023-06-21 13:43:49 +0700

    first commit

---
+++ ten.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style.css"/>
+<?php
+  $name = "สุรเชษฐ์ นรขุน";
+  $age = 17;
+  $year = 2566;
+  $byear = $year-$age;
+
+  echo "<h1> ยินดีต้อนรับkub </h1>";
+  echo "<h1>จัดทำโดย $name <br/></h1>";
+  echo "<h1>พ.ศ. เกิด $byear </h1><br/>";
+   echo " <h2> <img src='images/large.jpeg' /> </h1> <br/>";
+  
+
+?>
